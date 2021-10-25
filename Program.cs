22991c60c1a476d0c62eb943fc1d4bfc322117bd
GingerBeardx <ecryptographer@gmail.com>
2021-10-24 19:42:29 -0600

    Programatically created display board method. Created player class. Created game class. created player and board creation methods.

--- Program.cs
+++ Program.cs
@@ -1,40 +1,114 @@
 ﻿using System;
 
 Console.Title = "Tic-Tac-Toe";
+GameManager gameManager = new GameManager();
+gameManager.CreatePlayers();
+gameManager.CreateBoard();
+
+public class GameManager
+{
+    public Player PlayerOne { get; set; }
+    public Player PlayerTwo { get; set; }
+    public GameBoard Board { get; set; }
+    public bool IsGameRunning { get; set; }
+
+    public void CreatePlayers()
+    {
+        Console.Write("Please enter a name for player one: ");
+        string playerOneName = Console.ReadLine();
+        Console.Write("Please enter a name for player two: ");
+        string playerTwoName = Console.ReadLine();
+
+        PlayerOne = new Player(playerOneName, 'X');
+        PlayerTwo = new Player(playerTwoName, 'Y');
+    }
+
+    public void CreateBoard()
+    {
+        bool isValid = false;
+        int boardTiles = 0;
+        while (!isValid)
+        {
+            Console.Write("Please enter the number of tiles for the game board (should be a perfect square): ");
+            bool response = int.TryParse(Console.ReadLine(), out boardTiles);
+            if (Math.Sqrt(boardTiles) % 1 == 0)
+            {
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("That response is not a perfect square.");
+            }
+        }
+    }
+}
+
+public class Player
+{
+    public string FirstName { get; set; }
+    public int GamesWon { get; set; }
+    public char PlayerToken { get; set; }
+    public bool IsCurrentPlayer { get; set; }
+
+    public Player(string firstName, char playerToken)
+    {
+        FirstName = firstName;
+        GamesWon = 0;
+        PlayerToken = playerToken;
+    }
+
+
+    public void CurrentPlayer() => IsCurrentPlayer = true;
+    public void PlayerWon() => GamesWon++;
+}
 
 public class GameBoard
 {
     public GameTile[] Board { get; set; }
 
-    public GameBoard()
+    public GameBoard(int tiles)
     {
-        Board = new GameTile[9]
+        Board = new GameTile[tiles];
+        for (int i = 0; i < tiles; i++)
         {
-            new GameTile(0, 0), // Position 1
-            new GameTile(0, 1), // Position 2
-            new GameTile(0, 2), // Position 3
-            new GameTile(1, 0), // Position 4
-            new GameTile(1, 1), // Position 5
-            new GameTile(1, 2), // Position 6
-            new GameTile(2, 0), // Position 7
-            new GameTile(2, 1), // Position 8
-            new GameTile(2, 2) // Position 9
-        };
+            Board[i] = new GameTile();
+        }
+        
+    }
+
+    public void DrawGameBoard()
+    {
+        double boardSquareRoot = Math.Sqrt(Board.Length);
+        Console.WriteLine();
+        for (int i = 1; i <= Board.Length; i++)
+        {
+            if (i % boardSquareRoot == 0) 
+            {
+                Console.Write($"{Board[i - 1].TileDisplay()}");
+                Console.WriteLine();
+                for (int j = 1; j <= boardSquareRoot && i < Board.Length - (boardSquareRoot / 2); j++)
+                {
+                    if (j % boardSquareRoot != 0) Console.Write("---|");
+                    else Console.Write("---");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write($"{Board[i - 1].TileDisplay()}|");
+            }
+        }
     }
 }
 
 public class GameTile
 {
-    public int LocationX { get; set; }
-    public int LocationY { get; set; }
     public bool IsTokenX { get; set; }
     public bool IsTokenO { get; set; }
     public bool IsTokenBlank { get; set; }
 
-    public GameTile(int locationX, int locationY)
+    public GameTile()
     {
-        LocationX = locationX;
-        LocationY = locationY;
         IsTokenX = false;
         IsTokenO = false;
         IsTokenBlank = true;
@@ -57,5 +131,13 @@ public class GameTile
         IsTokenX = false;
         IsTokenO = false;
         IsTokenBlank = true;
+    }
+
+    public string TileDisplay()
+    {
+        
+        if (IsTokenO) return " O ";
+        if (IsTokenX) return " X ";
+        return "   ";
     }
 }
